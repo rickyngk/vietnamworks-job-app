1134fd0e99ba144d32dba42c155f2f891d00d667
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-29 11:23:07 +0700

    use fullscreen launcher

--- android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/LauncherActivity.java
+++ android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/LauncherActivity.java
@@ -1,7 +1,8 @@
 package vietnamworks.com.vietnamworksjobapp.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import vietnamworks.com.vietnamworksjobapp.R;
 
@@ -10,6 +11,11 @@ public class LauncherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemUI();
         setContentView(R.layout.activity_launcher);
+    }
+
+    private void hideSystemUI() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
