e166695074536ada8b2dd01374544e8859008d96
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-29 13:50:16 +0700

    add base activity helper to start new activity

--- android/VietnamworksJobApp/helper/src/main/java/vietnamworks/com/helper/BaseActivity.java
+++ android/VietnamworksJobApp/helper/src/main/java/vietnamworks/com/helper/BaseActivity.java
@@ -1,6 +1,9 @@
 package vietnamworks.com.helper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
@@ -8,6 +11,14 @@ import android.view.WindowManager;
  * Created by duynk on 12/29/15.
  */
 public class BaseActivity extends AppCompatActivity {
+    public static BaseActivity sInstance;
+    private Handler handler = new Handler();
+
+    public BaseActivity() {
+        super();
+        BaseActivity.sInstance = this;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +26,35 @@ public class BaseActivity extends AppCompatActivity {
 
     protected void hideSystemUI() {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void setTimeout(final Runnable f, long delay) {
+        handler.postDelayed(f, delay);
+    }
+
+    public void setTimeout(final Runnable f) {
+        handler.post(f);
+    }
+
+
+    public static void openActivity(Class<?> cls, @NonNull Bundle bundle) {
+        Intent intent = new Intent(BaseActivity.sInstance, cls);
+        intent.putExtras(bundle);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        BaseActivity.sInstance.startActivity(intent);
+    }
+
+    public static void openActivity(Class<?> cls) {
+        Intent intent = new Intent(BaseActivity.sInstance, cls);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        BaseActivity.sInstance.startActivity(intent);
+    }
+
+    public static void timeout(final Runnable f, long delay) {
+        sInstance.setTimeout(f, delay);
+    }
+
+    public static void timeout(final Runnable f) {
+        sInstance.setTimeout(f);
     }
 }
---
+++ android/VietnamworksJobApp/helper/src/main/java/vietnamworks/com/helper/Common.java
@@ -0,0 +1,8 @@
+package vietnamworks.com.helper;
+
+/**
+ * Created by duynk on 12/29/15.
+ */
+public class Common {
+
+}
