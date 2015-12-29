5bc068693160e33ec50e264c8be156fbbe508252
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-29 10:45:30 +0700

    generate android project

---
+++ android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/LauncherActivity.java
@@ -0,0 +1,15 @@
+package vietnamworks.com.vietnamworksjobapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vietnamworks.com.vietnamworksjobapp.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
