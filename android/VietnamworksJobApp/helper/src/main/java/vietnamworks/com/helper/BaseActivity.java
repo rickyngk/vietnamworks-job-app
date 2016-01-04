3be88c89fc1e544b3d3a66957ca8ce44cada4474
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-04 11:30:04 +0700

    detect layout changed

--- android/VietnamworksJobApp/helper/src/main/java/vietnamworks/com/helper/BaseActivity.java
+++ android/VietnamworksJobApp/helper/src/main/java/vietnamworks/com/helper/BaseActivity.java
@@ -1,10 +1,15 @@
 package vietnamworks.com.helper;
 
 import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 
 /**
@@ -14,6 +19,10 @@ public class BaseActivity extends AppCompatActivity {
     public static BaseActivity sInstance;
     private Handler handler = new Handler();
 
+    //detect layout changed
+    private boolean isKeyboardShown;
+    private Rect screenRegion;
+
     public BaseActivity() {
         super();
         BaseActivity.sInstance = this;
@@ -22,6 +31,23 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        isKeyboardShown = false;
+
+        final View activityRootView = getWindow().getDecorView().findViewById(android.R.id.content);
+        activityRootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                Rect r = new Rect();
+                getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+                boolean isSoftKeyShown = r.height() < getScreenHeight()*0.8;
+                isKeyboardShown = isSoftKeyShown;
+                screenRegion = r;
+                onLayoutChanged(r, isSoftKeyShown);
+            }
+        });
+    }
+
+    public void onLayoutChanged(Rect r, boolean isSoftKeyShown) {
     }
 
     protected void hideSystemUI() {
@@ -36,6 +62,10 @@ public class BaseActivity extends AppCompatActivity {
         handler.post(f);
     }
 
+    @Override
+    public void onBackPressed() {
+        this.moveTaskToBack(true);
+    }
 
     public static void openActivity(Class<?> cls, @NonNull Bundle bundle) {
         Intent intent = new Intent(BaseActivity.sInstance, cls);
@@ -56,5 +86,38 @@ public class BaseActivity extends AppCompatActivity {
 
     public static void timeout(final Runnable f) {
         sInstance.setTimeout(f);
+    }
+
+    public static int[] getScreenSize() {
+        Display display = BaseActivity.sInstance.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int s[] = new int[2];
+        s[0] = size.x;
+        s[1] = size.y;
+        return s;
+    }
+
+    public static int getScreenHeight() {
+        Display display = BaseActivity.sInstance.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.y;
+    }
+
+    public static int getScreenWidth() {
+        Display display = BaseActivity.sInstance.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        return size.x;
+    }
+
+    public static int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = BaseActivity.sInstance.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = BaseActivity.sInstance.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
