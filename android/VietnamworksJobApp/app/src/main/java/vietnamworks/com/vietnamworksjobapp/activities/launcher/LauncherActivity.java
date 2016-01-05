d4bc373b6be99aab136a3faba65020e84ac855c2
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-05 13:31:10 +0700

    skip welcome screen since second time

--- android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/launcher/LauncherActivity.java
+++ android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/launcher/LauncherActivity.java
@@ -2,17 +2,29 @@ package vietnamworks.com.vietnamworksjobapp.activities.launcher;
 
 import android.os.Bundle;
 
+import vietnamworks.com.vietnamworksjobapp.activities.main.MainActivity;
 import vietnamworks.com.vietnamworksjobapp.activities.onboarding.WelcomeActivity;
+import vietnamworks.com.vietnamworksjobapp.models.UserLocalProfileModel;
 
 public class LauncherActivity extends vietnamworks.com.vnwcore.LauncherActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        timeout(new Runnable() {
-            @Override
-            public void run() {
-                openActivity(WelcomeActivity.class);
-            }
-        }, 3000);
+
+        if (UserLocalProfileModel.loadLocal()) {
+            timeout(new Runnable() {
+                @Override
+                public void run() {
+                    openActivity(MainActivity.class);
+                }
+            }, 1000);
+        } else {
+            timeout(new Runnable() {
+                @Override
+                public void run() {
+                    openActivity(WelcomeActivity.class);
+                }
+            }, 1000);
+        }
     }
 }
