445e76dcb0e90da4cf259055ecc918c6263d0ccd
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-07 16:32:46 +0700

    auto complete job title

--- android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/onboarding/input/fragments/InputTitleFragment.java
+++ android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/onboarding/input/fragments/InputTitleFragment.java
@@ -1,18 +1,27 @@
 package vietnamworks.com.vietnamworksjobapp.activities.onboarding.input.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 
+import org.json.JSONArray;
+
+import R.helper.BaseFragment;
+import R.helper.Callback;
+import R.helper.CallbackResult;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import R.helper.BaseFragment;
 import vietnamworks.com.vietnamworksjobapp.R;
 import vietnamworks.com.vietnamworksjobapp.activities.onboarding.input.InputInfoActivity;
 import vietnamworks.com.vietnamworksjobapp.models.UserLocalProfileModel;
+import vietnamworks.com.vnwcore.VNWAPI;
 
 /**
  * Created by duynk on 1/4/16.
@@ -22,7 +31,12 @@ public class InputTitleFragment extends BaseFragment {
     Button btnNext;
 
     @Bind(R.id.job_title)
-    EditText jobTitle;
+    AutoCompleteTextView jobTitle;
+
+    ArrayAdapter<String> adapter;
+
+    private boolean isLoadingAutoHint = false;
+    private boolean preventTextChangeEvent = false;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -34,6 +48,52 @@ public class InputTitleFragment extends BaseFragment {
             public void onClick(View v) {
                 UserLocalProfileModel.getEntity().setJobTitle(jobTitle.getText().toString());
                 getActivityRef(InputInfoActivity.class).setPageIndex(1);
+            }
+        });
+
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line);
+
+
+        jobTitle.setAdapter(adapter);
+        jobTitle.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (isLoadingAutoHint || preventTextChangeEvent) {
+                    preventTextChangeEvent = false;
+                    return;
+                }
+                isLoadingAutoHint = true;
+                VNWAPI.jobTitleSuggestion(jobTitle.getText().toString(), new Callback() {
+                    @Override
+                    public void onCompleted(Context context, CallbackResult result) {
+                        adapter.clear();
+                        if (!result.hasError()) {
+                            try {
+                                JSONArray data = (JSONArray) result.getData();
+                                if (data != null) {
+                                    for (int i = 0; i < data.length(); i++) {
+                                        adapter.add(data.get(i).toString());
+                                    }
+                                }
+                                System.out.println(data);
+                                adapter.getFilter().filter(jobTitle.getText(), null);
+                            } catch (Exception E) {
+                                E.printStackTrace();
+                            }
+                        }
+                        isLoadingAutoHint = false;
+                    }
+                });
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
