a00331c7c4fa6cde21d94eda7e2f34bf5e8a6f80
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2016-01-18 11:34:15 +0700

    cont show job detail

--- android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/main/fragments/JobDetailFragment.java
+++ android/VietnamworksJobApp/app/src/main/java/vietnamworks/com/vietnamworksjobapp/activities/main/fragments/JobDetailFragment.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import R.helper.BaseFragment;
 import R.helper.Callback;
@@ -22,26 +25,34 @@ import vietnamworks.com.vnwcore.entities.Configuration;
 import vietnamworks.com.vnwcore.entities.Job;
 import vietnamworks.com.vnwcore.entities.JobDetail;
 import vietnamworks.com.vnwcore.entities.JobSummary;
+import vietnamworks.com.vnwcore.entities.Language;
+import vietnamworks.com.vnwcore.entities.Location;
 
 /**
  * Created by duynk on 1/15/16.
  */
 public class JobDetailFragment extends BaseFragment {
-    @Bind(R.id.job_detail_job_title)
-    TextView jobTitle;
+    @Bind(R.id.job_detail_job_title) TextView jobTitle;
 
-    @Bind(R.id.job_detail_job_level)
-    TextView jobLevel;
+    //head
+    @Bind(R.id.job_detail_company_name) TextView companyName;
+    @Bind(R.id.job_detail_company_image) ImageView companyImage;
+    @Bind(R.id.job_detail_company_address) TextView companyAddress;
 
-    @Bind(R.id.job_detail_job_industry)
-    TextView jobIndustry;
+    //job summary
+    @Bind(R.id.summary_job_detail) View summaryJobDetailView;
+    @Bind(R.id.job_detail_working_location) TextView workingLocations;
+    @Bind(R.id.job_detail_job_level) TextView jobLevel;
+    @Bind(R.id.job_detail_job_industry) TextView jobIndustry;
+    @Bind(R.id.job_detail_preferred_language) TextView preferredLanguage;
 
-    @Bind(R.id.job_detail_company_name)
-    TextView companyName;
-
-    @Bind(R.id.job_detail_company_profile)
-    TextView companyProfile;
-
+    //company detail
+    @Bind(R.id.about_company) View aboutCompanyView;
+    @Bind(R.id.company_detail_name) TextView companyDetailName;
+    @Bind(R.id.company_detail_address) TextView companyDetailAddress;
+    @Bind(R.id.company_detail_contact) TextView companyDetailContact;
+    @Bind(R.id.company_detail_desc) TextView companyDetailDesc;
+    @Bind(R.id.company_detail_size) TextView companyDetailSize;
 
     @Bind(R.id.progressBar)
     ProgressBar progressBar;
@@ -59,22 +70,45 @@ public class JobDetailFragment extends BaseFragment {
         jobTitle.setText(bundle.getString("jobTitle"));
         String jobId = bundle.getString("jobId");
 
-        jobLevel.setText("");
-        jobIndustry.setText("");
+        workingLocations.setText("");
         companyName.setText("");
-        companyProfile.setText("");
+        companyAddress.setText("");
+
+        summaryJobDetailView.setVisibility(View.INVISIBLE);
+        aboutCompanyView.setVisibility(View.INVISIBLE);
 
         progressBar.setVisibility(View.VISIBLE);
         JobDetailModel.load(getContext(), jobId, new Callback() {
             @Override
             public void onCompleted(Context context, CallbackResult result) {
                 progressBar.setVisibility(View.GONE);
+                summaryJobDetailView.setVisibility(View.VISIBLE);
+                aboutCompanyView.setVisibility(View.VISIBLE);
                 if (!result.hasError()) {
-                    Job j = (Job) result.getData();
-                    JobDetail jd = j.getJobDetail();
-                    JobSummary js = j.getJobSummary();
-                    Company c = j.getCompany();
                     if (JobDetailFragment.this.getContext() == context) {
+                        Job j = (Job) result.getData();
+                        JobDetail jd = j.getJobDetail();
+
+                        //job summary
+                        JobSummary js = j.getJobSummary();
+
+                        String locations = js.getLocations();
+                        if (locations != null && !locations.isEmpty()) {
+                            String[] arr = locations.split(",");
+                            StringBuilder b = new StringBuilder();
+                            String delim = "";
+                            for (String s : arr) {
+                                Location obj = Configuration.findLocation(s.trim());
+                                if (obj != null) {
+                                    b.append(delim);
+                                    b.append(obj.getEn());
+                                    delim = ", ";
+                                }
+                            }
+                            workingLocations.setText(String.format(getString(R.string.work_location), b.toString()));
+                        }
+
+
                         jobLevel.setText(Configuration.findJobLevel(js.getLevel() + "").getEn());
 
                         String industry = js.getCategories();
@@ -93,8 +127,23 @@ public class JobDetailFragment extends BaseFragment {
                             jobIndustry.setText(b.toString());
                         }
 
+                        Language lang = Configuration.findLanguage(js.getPreferLanguage() + "");
+                        if (lang != null) {
+                            preferredLanguage.setText(lang.getName());
+                        }
+
+                        //company
+                        Company c = j.getCompany();
                         companyName.setText(c.getName());
-                        companyProfile.setText(c.getProfile());
+                        companyAddress.setText(c.getAddress());
+                        Picasso.with(context).load(c.getLogo()).into(companyImage);
+
+                        //company detail
+                        companyDetailName.setText(c.getName());
+                        companyDetailAddress.setText(c.getAddress());
+                        companyDetailContact.setText(String.format(getString(R.string.contact_person), c.getContactPerson()));
+                        companyDetailDesc.setText(c.getProfile());
+                        companyDetailSize.setText(String.format(getString(R.string.company_size), c.getSize()));
                     }
                 }
             }
